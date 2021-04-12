3e9a8ead188b334c35432e154ffc9292a01db3e3
sealockej15 <46949327+sealockej15@users.noreply.github.com>
2021-04-12 20:04:21 +0200

    Add files via upload

---
+++ tutorial.js
@@ -0,0 +1,23 @@
+let person = {
+    name: "Jalen",
+    age: 13,
+    favFood: "Hotpot",
+    fun: function(){
+        console.log("Ich spreche Deutsch");
+    }
+}
+{
+    let slicesOfPizza = 10;
+    console.log(slicesOfPizza + 10)
+}
+    //primitive
+    //objects
+
+
+
+//This is really important
+/*
+    This
+    is a multiline
+    comment
+*/
